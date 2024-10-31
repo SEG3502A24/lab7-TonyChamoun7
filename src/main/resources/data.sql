c645dbc7b2fb41dae5fc75e579c4a4093d6e6f9e
TonyChamoun7 <cooltony7@hotmail.com>
2024-10-30 23:10:38 -0400

    changes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO book (title, category, isbn, cost, year, description)
+VALUES
+('Database Book', 'Technology', '1122334455', 29.99, 2020, 'A book about databases'),
+('Spring Boot Guide', 'Technology', '2233445566', 35.99, 2021, 'Guide to Spring Boot');
