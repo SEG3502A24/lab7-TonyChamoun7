9a58b0cfb29ed96c1f2b4f8de8b1efcf7d0e6902
TonyChamoun7 <cooltony7@hotmail.com>
2024-11-02 12:24:44 -0400

    changes

--- src/main/kotlin/seg3x02/booksrestapi/assemblers/OrderModelAssembler.kt
+++ src/main/kotlin/seg3x02/booksrestapi/assemblers/OrderModelAssembler.kt
@@ -1,33 +1,22 @@
 package seg3x02.booksrestapi.assemblers
 
-import OrderRepresentation
-import org.springframework.hateoas.CollectionModel
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
 import org.springframework.stereotype.Component
 import seg3x02.booksrestapi.controller.ApiController
 import seg3x02.booksrestapi.entities.Order
+import seg3x02.booksrestapi.representation.OrderRepresentation
 
 @Component
-class OrderModelAssembler: RepresentationModelAssemblerSupport<Order,
-        OrderRepresentation>(ApiController::class.java, OrderRepresentation::class.java) {
+class OrderModelAssembler : RepresentationModelAssemblerSupport<Order, OrderRepresentation>(
+    ApiController::class.java, OrderRepresentation::class.java
+) {
     override fun toModel(entity: Order): OrderRepresentation {
         val orderRepresentation = instantiateModel(entity)
         orderRepresentation.add(
-            WebMvcLinkBuilder.linkTo(
-            WebMvcLinkBuilder.methodOn(ApiController::class.java)
-                .getOrderById(entity.id))
-            .withSelfRel())
-        orderRepresentation.id = entity.id
-        orderRepresentation.quantity = entity.quantity
+            linkTo(ApiController::class.java)
+                .slash("orders/${entity.id}").withSelfRel()
+        )
         return orderRepresentation
-    }
-
-    fun toCollectionModel(entities: Set<Order>): CollectionModel<OrderRepresentation> {
-        val orderRepresentations = super.toCollectionModel(entities)
-        orderRepresentations.add(WebMvcLinkBuilder.linkTo(
-            WebMvcLinkBuilder.methodOn(
-                ApiController::class.java)).withRel("orders"))
-        return orderRepresentations
     }
 }
